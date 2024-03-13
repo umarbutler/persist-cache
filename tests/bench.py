f5e9cf1d25dc3f49a0af30fc5b861d2fe32675b5
Umar Butler <umar@umar.au>
2024-03-13 23:00:41 +1100

    Added disk space calculations to benchmarks.

--- tests/bench.py
+++ tests/bench.py
@@ -1,6 +1,8 @@
 """Benchmark persist-cache against other popular persistent caching libraries."""
 import random
 import time
+import os
+import shutil
 from typing import Callable
 
 import cachier
@@ -8,6 +10,26 @@ import locache
 from joblib import Memory
 
 import persist_cache
+
+
+def get_size(path: str) -> int:
+    """Get the size of a file or directory."""
+    
+    if os.path.isfile(path):
+        return os.path.getsize(path)
+    
+    size = 0
+    
+    for subpath in os.listdir(path):
+        subpath = os.path.join(path, subpath)
+        
+        if os.path.isfile(subpath):
+            size += os.path.getsize(subpath)
+        
+        else:
+            size += get_size(subpath)
+    
+    return size
 
 
 def time_consuming_function(seed: int) -> float:
@@ -29,36 +51,40 @@ def time_function(func: Callable, iterations: int) -> float:
 # Initialise a `joblib.Memory` instance.
 memory = Memory(".joblib", verbose=0)
 
-# Map cachers to themselves wrapped around the time consuming function and functions to remove their caches.
+# Map cachers to themselves wrapped around the time consuming function, functions to remove their caches and the names of their cache directories.
 cachers = {
     'persist-cache': (
         persist_cache.cache(time_consuming_function),
         lambda cache: cache.clear_cache(),
+        '.persist_cache',
     ),
 
     'cachier': (
         cachier.cachier(cache_dir='.cachier')(time_consuming_function),
         lambda cache: cache.clear_cache(),
+        '.cachier',
     ),
     
     'joblib': (
         memory.cache(time_consuming_function),
         lambda _: memory.clear(),
+        '.joblib',
     ),
     
     'locache': (
         locache.persist(time_consuming_function),
-        lambda _: ...,
+        lambda _: shutil.rmtree('bench.cache', ignore_errors=True) if os.path.exists('bench.cache') else None,
+        'bench.cache',
     )
 }
 
 if __name__ == '__main__':
     benchmarks = []
-    iterations = 5000
+    iterations = 1000
     
     uncached_time = time_function(time_consuming_function, iterations)/iterations
     
-    for cacher, (func, emptier) in cachers.items():
+    for cacher, (func, emptier, dir) in cachers.items():
         print(f'=== {cacher} ===')
         # Empty the cache.
         emptier(func)
@@ -70,3 +96,8 @@ if __name__ == '__main__':
         # Time how long it takes to retrieve a cached function call.
         get_time = time_function(func, iterations)
         print(f'Average get time: {get_time/iterations} seconds')
+
+        # Get the size of the cache.
+        print(f'Bytes used: {get_size(dir)}')
+        
+        print()
